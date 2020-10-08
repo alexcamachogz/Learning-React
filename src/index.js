7580b9d3692f6b0b464721c6dce3c4b13bb7428c
Alejandra Camacho <camacho.ale94@gmail.com>
2020-10-07 23:06:18 -0500

    Add eslint standard

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const hello = <h1>Hello World!</h1>
+
+const divRoot = document.querySelector('#root')
+
+ReactDOM.render(hello, divRoot)
