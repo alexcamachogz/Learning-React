a83c6e3fc1e5d98120ec0399e28ee1577ff514bc
Alejandra Camacho <camacho.ale94@gmail.com>
2020-10-10 21:16:37 -0500

    Add reset & substract methods

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,13 +1,23 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
-const CounterApp = ({ value }) => {
-  const [counter, setCounter] = useState(0)
+const CounterApp = ({ value = 0 }) => {
+  const [counter, setCounter] = useState(value)
 
   // handleAdd
   const handleAdd = (e) => {
     setCounter(counter + 1)
     // setCounter((c) => c + 2)
+  }
+
+  // handleSubstract
+  const handleSubstract = (e) => {
+    setCounter(counter - 1)
+  }
+
+  // handleReset
+  const handleReset = (e) => {
+    setCounter(value)
   }
 
   return (
@@ -21,6 +31,8 @@ const CounterApp = ({ value }) => {
       <h2>{counter}</h2>
 
       <button onClick={handleAdd}>+1</button>
+      <button onClick={handleReset}>Reset</button>
+      <button onClick={handleSubstract}>-1</button>
     </>
   )
 }
